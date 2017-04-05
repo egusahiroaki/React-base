25c016cac85ed4c877c4e944afd2234fb33bafb5
egusa hiroaki <egusa.hiroaki@gmail.com>
2017-04-06 05:21:06 +0900

    追加

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+  entry: {js:'./src/main.js'},
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port:8080,
+    inline: true,
+    historyApiFallback: true,
+  }
+};
